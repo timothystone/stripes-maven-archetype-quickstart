564a48d53b1cf238f5611c730d7d5ba6d6477191
timothystone <tstone@petmystone.com>
2012-02-12 21:24:05 -0500

    Update copyright. Frederic Daoud wrote the code; Chaminda Amarasinghe put the code on sourceforge under an incorrect license. The original code was released under the Apache Software License 2.0. The license will be updated to the ASL.

--- src/main/resources/archetype-resources/src/main/java/HomeActionBean.java
+++ src/main/resources/archetype-resources/src/main/java/HomeActionBean.java
@@ -1,6 +1,6 @@
 /*
  * 
- * Copyright (C) 2008-2012 Chaminda Amarasinghe
+ * Copyright (C) 2008-2012 Freddy Daoud
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the 
  * GNU General Public License as published by the Free Software Foundation; either version 2 of the License, 
--- src/main/resources/archetype-resources/src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/resources/archetype-resources/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -1,5 +1,5 @@
 <%--
-Copyright (C) 2008-2012 Chaminda Amarasinghe
+Copyright (C) 2008-2012 Freddy Daoud
 
 This program is free software; you can redistribute it and/or modify it under the terms of the 
 GNU General Public License as published by the Free Software Foundation; either version 2 of the License, 
